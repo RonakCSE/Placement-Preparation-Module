465dee75d05e06561c28717fdee7c4c159088cbe
Ronak Raj <ronak072426@gmail.com>
2023-08-09 23:47:29 +0530

    commit

--- Week-1/Day 2: Arrays Part-II/Count Inversion.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean isIdealPermutation(int[] nums) {
-       for(int i = 0; i < nums.length; i ++) 
-           if(i - nums[i] > 1 || i - nums[i] < - 1) {
-                return false;
-           }
-           return true;
-    }
-}
